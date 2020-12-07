9fc6b33ef4a4c2865bf456d5e7e149ef51af9bef
Dashnyam <lord_dashka277@yahoo.com>
2020-12-07 22:16:44 +0800

    Updates

---
+++ precache-manifest.dc3106bae986bdd99a4c6db3401c38ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c0d58cd9d418e613971eb69d61d5f6",
+    "url": "/1234burger/index.html"
+  },
+  {
+    "revision": "4cdac98b184f0c813904",
+    "url": "/1234burger/static/css/main.72853416.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/1234burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/1234burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cdac98b184f0c813904",
+    "url": "/1234burger/static/js/main.92b077d5.chunk.js"
+  },
+  {
+    "revision": "920322a51f23f6034d4e",
+    "url": "/1234burger/static/js/runtime-main.af7175c6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/1234burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
